8a8108105deec2d3478a274734292a3f0e03ea26
bbanez <branislav.vaj@gmail.com>
2026-02-04 13:25:54 +0100

    fix go test

--- tests/pack_unpack_test.go
+++ tests/pack_unpack_test.go
@@ -7,33 +7,33 @@ import (
 	"time"
 
 	"github.com/bbanez/minibin/src/utils"
-	minibin "github.com/bbanez/minibin/tests/dist"
+	m "github.com/bbanez/minibin/tests/dist/go"
 )
 
 func TestPack(t *testing.T) {
-	items := minibin.Obj1Arr{
-		Items: []*minibin.Obj1{},
+	items := m.Obj1Arr{
+		Items: []*m.Obj1{},
 	}
 	for i := 0; i < 1; i++ {
-		item := &minibin.Obj1{
+		item := &m.Obj1{
 			Str:    fmt.Sprintf("item_%d", i),
 			I32:    int32(i * 10),
 			I32Arr: []int32{int32(i * 10), int32(i * 20), int32(i * 30)},
-			I64:    int64(i * 100),
+			I64:    int64(-i * 100),
 			I64Arr: []int64{int64(i * 100), int64(i * 200), int64(i * 300)},
 			U32:    uint32(i * 1000),
 			U32Arr: []uint32{uint32(i * 1000), uint32(i * 2000), uint32(i * 3000)},
 			U64:    uint64(i * 10000),
 			U64Arr: []uint64{uint64(i * 10000), uint64(i * 20000), uint64(i * 30000)},
-			F32:    float32(i) + 0.123,
-			F32Arr: []float32{float32(i) + 0.234, float32(i) + 0.345},
+			F32:    -float32((i + 1)) - 0.123,
+			F32Arr: []float32{float32(i) + 0.234, float32(-i) + 0.345},
 			F64:    float64(i) + 0.456789,
 			F64Arr: []float64{float64(i) + 0.567890, float64(i) + 0.678901},
-			Obj2: minibin.Obj2{
+			Obj2: m.Obj2{
 				Key:   fmt.Sprintf("key_%d", i),
 				Value: float32(i) * 1.5,
 			},
-			Obj2Arr: []*minibin.Obj2{
+			Obj2Arr: []*m.Obj2{
 				{
 					Key:   fmt.Sprintf("keyarr_%d_1", i),
 					Value: float32(i) * 2.5,
@@ -43,8 +43,8 @@ func TestPack(t *testing.T) {
 					Value: float32(i) * 3.5,
 				},
 			},
-			Enum1:    minibin.ENUM1_E1,
-			Enum1Arr: []minibin.Enum1{minibin.ENUM1_E2, minibin.ENUM1_E3},
+			Enum1:    m.ENUM1_E1,
+			Enum1Arr: []m.Enum1{m.ENUM1_E2, m.ENUM1_E3},
 		}
 		items.Items = append(items.Items, item)
 	}
@@ -55,13 +55,13 @@ func TestPack(t *testing.T) {
 	bytes := items.Pack()
 	fmt.Println("MB time", time.Now().UnixNano()-timeOffset, "-> size:", len(bytes))
 	timeOffset = time.Now().UnixNano()
-	e, err := minibin.UnpackObj1Arr(bytes)
+	e, err := m.UnpackObj1Arr(bytes, nil)
 	fmt.Println("MB Unpack time", time.Now().UnixNano()-timeOffset)
 	if err != nil {
 		t.Fatal("Failed to unpack entry:", err)
 	}
 	timeOffset = time.Now().UnixNano()
-	tmp := minibin.Obj1Arr{}
+	tmp := m.Obj1Arr{}
 	err = json.Unmarshal([]byte(s1), &tmp)
 	fmt.Println("JS Unpack time", time.Now().UnixNano()-timeOffset)
 	if err != nil {
@@ -69,6 +69,8 @@ func TestPack(t *testing.T) {
 	}
 	s2 := utils.SerializeJson(e)
 	if s1 != s2 {
+		fmt.Println("S1", s1)
+		fmt.Println("S2", s2)
 		t.Errorf("Object do not match after pack/unpack")
 	}
 }
